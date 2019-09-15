2d851ba1d841182f7230703df5edefd4ac148e02
Bharat Kashyap <bharatkashyap@outlook.com>
2019-09-15 07:07:33 -0400

    fix: All endpoints and seller/buyer data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const crypto = require("crypto");
 //const airtable = require('airtable');
 const algosdk = require('algosdk');
 
@@ -9,26 +10,107 @@ const aport = 9100;
 const us = "CXY4WAAPMNWEFHNAHEGZBZPCKZPKKVXN3TG6RNC6LABFHRM6C3274ZFAEM"
 const mnem_us = "faculty scissors basket wish embrace verb narrow front canal enemy melt pencil crane lock success poem remember force ring dream salmon twice attack about fun";
 
+const divisionFactor = 0.01;
+
 const store =
 {
-
     "sellers": [
         {
-            "id": "R2BTV362CWRMCIC3HSRLWDNBLIFGCHSFSGMDLVMQEFBVP4OPL6EXECQWGA",
+            "id": "ODVLQILOZCMY5FNINSIMEYKWGXQUXQP27IIJBGBIG4LFXX6GAGH4PSIL2I",
             "buyers": [],
-            "transactions": []
+            "transactions": [
+                {
+                    "name": "MIT Transit Lab",
+                    "amount": 25000000
+                },
+                {
+                    "name": "Massport",
+                    "amount": 32500000
+                },
+                {
+                    "name": "Street light survey - City of Cambridge",
+                    "amount": 12500000
+                }
+            ]
+        },
+        {
+            "id": "5SSKQXWACBNY2G4M5PUBZOZOBBBRTHYUZQ3UD2OEMOGFCXJEMLDAK7ZE7M",
+            "buyers": [],
+            "transactions": [
+                {
+                    "name": "MIT Transit Lab",
+                    "amount": 25000000
+                },
+                {
+                    "name": "Massport",
+                    "amount": 32500000
+                },
+                {
+                    "name": "Street light survey - City of Cambridge",
+                    "amount": 12500000
+                }
+            ]
+        },
+        {
+            "id": "QYIAZNEV7DQGNZ2QSFVEBRDDFAL4CALFTN5VQ2QZKYZ4YB7THGFSP6DT2A",
+            "buyers": [],
+            "transactions": [
+                {
+                    "name": "Massport",
+                    "amount": 32500000
+                },
+                {
+                    "name": "Street light survey - City of Cambridge",
+                    "amount": 12500000
+                }
+            ]
+        },
+        {
+            "id": "PPVZQQLXRR6NUOVIDRCQCD7NDQLPVCEEJI5NGBW2RFOD3WYN6VQ3WMDW34",
+            "buyers": [],
+            "transactions": [
+                {
+                    "name": "Street light survey - City of Cambridge",
+                    "amount": 12500000
+                }
+            ]
         }
     ],
-    "buyers": [
-        {
-            "id": "LQUF6YRKBMJSUPUTWMPLWR66GGC4PARNSUPR2ELFJIPD4NZWTPL47SUDXM",
-            "name": "",
+    "buyers": {
+        "LQUF6YRKBMJSUPUTWMPLWR66GGC4PARNSUPR2ELFJIPD4NZWTPL47SUDXM": {
+            "name": "MIT Transit Lab",
+            "mnem": "",
+            "transactions": [
+                {
+                    "key": "ca6b76bb6b71408a35caec55af836a319fda6b8a"
+                }
+            ]
+        },
+        "AXBVGTXWLPSSY6CRU3QQ3RSWRLQNOGTKI3U2346MRHJJN6NGHT72O37YV4": {
+            "name": "Massport",
+            "mnem": "",
+            "transactions": [
+                {
+                    "key": "3fb3c378b2d9d47d08f1a8b9682fb70dc4733914"
+                }
+            ]
+        },
+        "3ZTFRHYZDB37LQUKXQUVEXH5BPKABIRTINCVRUXGFK36ICDZ2P2P4EFRJQ": {
+            "name": "Street light survey - City of Cambridge",
+            "mnem": "",
+            "transactions": [
+                {
+                    "key": "f616e3b8de79ab821fbad6d21d79411c9d5a3963"
+                }
+            ]
+        },
+        "Y3GJ5UQZKETGOGXU43CZQFB2RD3FCPNXVOXIE5AG7SHMOE2FXQZCVAWK3A": {
+            "name": "Massachusetts Bay Transportation Authority",
             "mnem": "",
             "transactions": []
         }
-    ],
+    },
     "transactions": []
-    
 }
 
 
@@ -126,40 +208,50 @@ function fetchUsers(req, res) {
 }
 
 async function doTransaction(req, res) {
-    let { from, to, algos, mnem, name, meta } = req.body;
+    let { from, algos, mnem, meta } = req.body;
     // from is Buter
     // to is Seller
+    
     const algodclient = new algosdk.Algod(atoken, aserver, aport);
     let fr = await algodclient.status();
     let fround = fr.lastRound;
     let lround = fround + 1000;
-    let apiKey = 5;
+    let apiKey = crypto.randomBytes(20).toString('hex');
 
-    txnToSeller = {
-        "from": from,
-        "to": to,
-        "fee": 1000,
-        "amount": algos,
-        "firstRound": fround,
-        "lastRound": lround,
-        "note": algosdk.encodeObj(name),
-        "genesisID": "testnet-v1.0",
-        "genesisHash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
-    };
+    let name = store.buyers[from] && store.buyers[from].name;
+
+    store.sellers.forEach(async (seller) => {
+        
+        let to = seller.id;
+
+        txnToSeller = {
+            "from": from,
+            "to": to,
+            "fee": 1000,
+            "amount": algos*divisionFactor,
+            "firstRound": fround,
+            "lastRound": lround,
+            "note": algosdk.encodeObj(name),
+            "genesisID": "testnet-v1.0",
+            "genesisHash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
+        };
+
+        let recovered_account = algosdk.mnemonicToSecretKey(mnem);
+        let secretKey = recovered_account.sk;
+
+        var signedTxnToSeller = algosdk.signTransaction(txnToSeller, secretKey);
+
+        try {
+            let tx = await algodclient.sendRawTransaction(signedTxnToSeller.blob);
+            var textedJson = JSON.stringify(tx, undefined, 4);
+            seller.transactions.push({"name": name, "amount": algos});
+        } catch (e) {   
+            console.log(e);
+        };
+    });
 
     let recovered_account = algosdk.mnemonicToSecretKey(mnem);
     let secretKey = recovered_account.sk;
-
-    var signedTxnToSeller = algosdk.signTransaction(txnToSeller, secretKey);
-
-    try {
-        let tx = await algodclient.sendRawTransaction(signedTxnToSeller.blob);
-        var textedJson = JSON.stringify(tx, undefined, 4);
-        let seller = store.sellers.find(t => t.id === to);
-        seller.transactions.push({"name": name, "amount": algos});
-    } catch (e) {   
-        console.log(e);
-    };
 
     txnToUs = {
         "from": from,
@@ -201,7 +293,8 @@ async function doTransaction(req, res) {
     try {
         let tx = await algodclient.sendRawTransaction(signedTxnToBuyer.blob);
         var textedJson = JSON.stringify(tx, undefined, 4);
-        let buyer = store.buyers.find(t => t.id === from);
+        //let buyer = store.buyers.find(t => t.id === from);
+        let buyer = store.buyers[from];
         buyer.transactions.push( {"key": apiKey, "meta": meta});
         res.status(200).send(store);
     } catch (e) {   
@@ -210,17 +303,22 @@ async function doTransaction(req, res) {
 }
 
 async function fetchTransactions(req, res) {
-    const user = req.body.account;
-    const type = req.body.type;
+    const account = req.query.account;
+    const type = req.query.type;
     let seller, buyer;
-    if(type === "seller") seller = store.sellers.find(t => t.id === user);
-    else buyer = store.buyers.find(t => t.id === user);
+    if(type === "seller") seller = store.sellers.find(t => t.id === account);
+    else buyer = store.buyers[account];
+    
     try {
         const algodclient = new algosdk.Algod(atoken, aserver, aport);
         let tx = await algodclient.accountInformation(account);
         let textedJson = JSON.stringify(tx, undefined, 4);
-        //console.log(textedJson);
-        let response = {"transactions": seller.transactions, "balance": textedJson.amount }
+        let response = {};
+
+        console.log(seller, buyer);
+        if(seller) response["transactions"] = seller.transactions;
+        else response["transactions"] = buyer.transactions;
+        response["amount"] = textedJson.amount;
         res.status(200).send(response);
     } catch (e) {
         console.log(e);
